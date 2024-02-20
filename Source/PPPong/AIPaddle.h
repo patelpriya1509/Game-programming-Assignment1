783b4f7f651c0f399ab5997b75add4e0f98f8ada
patelpriya1509 <133623717+patelpriya1509@users.noreply.github.com>
2024-02-20 13:37:15 -0500

    Add files via upload
    Created a new project since the classes for the last one wasn't correct

---
+++ Source/PPPong/AIPaddle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerPaddle.h"
+#include "AIPaddle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PPPONG_API AAIPaddle : public APlayerPaddle
+{
+	GENERATED_BODY()
+	
+};
